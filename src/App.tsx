f5f533c5acccf3fc763bc81e81f1bf100b0c0b92
Matvei <mat3580@gmail.com>
2022-03-26 23:32:32 +0300

    init routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Link, Outlet} from 'react-router-dom';
+
+export function List() {
+  return <div>list</div>
+}
+
+export function Details() {
+  return <div>details</div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/purchase">Purchase</Link>
+          </li>
+          <li>
+            <Link to="/my-orders">My Orders</Link>
+          </li>
+          <li>
+            <Link to="/sell">Sell</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet/>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App, {Details, List} from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App/>}>
+          <Route path="/:id" element={<Details/>}/>
+          <Route path="/" element={<List/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
