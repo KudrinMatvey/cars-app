24e5191ca57dd7e4dd67bdaf0db3794007a39bd6
Matvei <mat3580@gmail.com>
2022-03-30 21:13:32 +0300

    autofix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.scss';
 
-import {Outlet} from 'react-router-dom';
-import {Header} from './components/Header';
-import {Footer} from './components/Footer';
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
